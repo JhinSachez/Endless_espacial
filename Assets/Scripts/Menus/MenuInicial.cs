d440ffb9a75b793454bc6579f0d065ba18fa579a
Ruben Arias <zap483@amerike.edu.mx>
2024-10-05 20:37:41 -0600

    CambioUi

--- Assets/Scripts/Menus/MenuInicial.cs
+++ Assets/Scripts/Menus/MenuInicial.cs
@@ -12,7 +12,7 @@ public class MenuInicial : MonoBehaviour
 
     public void Salir()
     {
-        Debug.Log("Saliendo...");
+        UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 
